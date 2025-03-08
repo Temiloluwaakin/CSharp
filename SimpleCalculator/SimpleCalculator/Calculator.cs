0769aca0729e99dfeb0b8636315d111be526c4e6
Temiloluwaakin <106915700+Temiloluwaakin@users.noreply.github.com>
2025-03-09 00:45:07 +0100

    first commit wuth .gitignore to exclude Visual Studio files

---
+++ SimpleCalculator/SimpleCalculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Calculator
+    {
+        public double Calculate(double num1, double num2, char operation)
+        {
+            if (operation == '+')
+            {
+                return num1 + num2;
+            }
+            else if (operation == '-')
+            {
+                return num1 - num2;
+            }
+            else if (operation == '*')
+            {
+                return num1 * num2;
+            }
+            else if (operation == '/')
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Error: Cannot divide by zero.");
+                    return double.NaN; // Not a number
+                }
+                return num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator.");
+                return double.NaN;
+            }
+        }
+
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using SimpleCalculator;
+
+public class Program
+{
+    static void Main()
+    {
+        Calculator calculator = new Calculator();
+        {
+            Console.WriteLine("Enter first number");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operator (+, -, *, /): ");
+            char operation = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            double result = calculator.Calculate(input1, input2, operation);
+            Console.WriteLine($"Result: {result}");
+           
+        }
+    }
+}
